eeff86a3aa2b5045543d6eaf44e29a020cdd9016
andrewWong21 <awong541@gmail.com>
2023-11-06 14:48:02 -0500

    Day 6 - Sum Square Difference

---
+++ _06_SumSquareDifference.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class _05_SmallestMultiple{
+    
+    public static void main(String[] args){
+        int sum = 0;
+        int sumSq = 0;
+        for (int i = 1; i <= 100; i++){
+            sum += i;
+            sumSq += i * i;
+        }
+        System.out.println("The square of the sum of the first 100 natural numbers is " + sum * sum);
+        System.out.println("The sum of the squares of the first 100 natural numbers is " + sumSq);
+        System.out.println(String.format("Difference: %d - %d = %d", sum * sum, sumSq, sum * sum - sumSq));
+        
+    }
+}
